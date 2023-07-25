3686048f8b5148403ff9696146b498078243b7f3
kuljeet singh <kuljeet9186@gmail.com>
2023-07-25 15:59:11 +0530

    Update categoryRoutes.js

--- routes/categoryRoutes.js
+++ routes/categoryRoutes.js
@@ -1,40 +1,45 @@
+
 import express from "express";
-import color from "colors";
-import dotenv from "dotenv";
-import morgan from "morgan";
-import connectDB from "../config/db.js";
-import authRoutes from "../routes/authRoute.js";
-import categoryRoutes from "../routes/categoryRoutes.js";
-import productRoutes from "../routes/productRoutes.js";
-import cors from "cors";
-import path from 'path'
-import { fileURLTopath} from 'url';
+import { isAdmin, requireSignIn } from "./../middlewares/authMiddleware.js";
+import {
+  categoryControlller,
+  createCategoryController,
+  deleteCategoryCOntroller,
+  singleCategoryController,
+  updateCategoryController,
+} from "./../controllers/categoryController.js";
 
+const router = express.Router();
 
-dotenv.config();
-connectDB();
-//rest object 
-const app=express();
+//routes
+// create category
+router.post(
+  "/create-category",
+  requireSignIn,
+  isAdmin,
+  createCategoryController
+);
 
+//update category
+router.put(
+  "/update-category/:id",
+  requireSignIn,
+  isAdmin,
+  updateCategoryController
+);
 
-const __filename=fileURLToPath(import.meta.url);
-const __dirname=path.dirname(__filename);
+//getALl category
+router.get("/get-category", categoryControlller);
 
-//middlewares
-app.use(cors())
-app.use(express.json())
-app.use(morgan('dev'))
-app.use(express.static(path.join(__dirname,'./client/build')))
+//single category
+router.get("/single-category/:slug", singleCategoryController);
 
-app.use('/api/v1/auth',authRoutes);
-app.use('/api/v1/category',categoryRoutes);
-app.use('/api/v1/product',productRoutes);
-const PORT = process.env.PORT || 8080;
+//delete category
+router.delete(
+  "/delete-category/:id",
+  requireSignIn,
+  isAdmin,
+  deleteCategoryCOntroller
+);
 
-app.use('*',function(req,res){
-    res.sendFile(path.join(__dirname,"./client/build/index.html"))
-})
-
-app.listen(PORT,()=>{
-    console.log(`server is running on ${process.env.DEV_MODE} mode on port ${PORT}`.bgCyan.white);
-});
+export default router;
